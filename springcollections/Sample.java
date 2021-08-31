396db76f5a4fdfce690dc65481ed22e891ed5b8d
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2021-08-31 17:19:48 +0530

    Add files via upload

---
+++ springcollections/Sample.java
@@ -0,0 +1,16 @@
+package springcollections;
+
+public class Sample {
+	
+	public void display() {
+		System.out.println("inside the sample test..");
+	}
+	
+	public void initmethod() {
+		System.out.println("initalization is called....");
+	}
+
+	public void destroy() {
+		System.out.println("Destroy method is called....");
+	}
+}
