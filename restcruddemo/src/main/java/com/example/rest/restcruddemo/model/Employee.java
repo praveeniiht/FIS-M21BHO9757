7ebc33a05c9ab3ab24361a762e997065ad44568d
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2021-09-07 15:03:32 +0530

    Add files via upload
    Springboot REST Application for CRUD Opeations

---
+++ restcruddemo/src/main/java/com/example/rest/restcruddemo/model/Employee.java
@@ -0,0 +1,60 @@
+package com.example.rest.restcruddemo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	int empid;
+	
+	@Column
+	String name;
+	
+	@Column
+	String dept;
+	
+	@Column
+	String desg;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public String getDesg() {
+		return desg;
+	}
+	public void setDesg(String desg) {
+		this.desg = desg;
+	}
+	public Employee(String name, String dept, String desg) {
+		super();
+		this.name = name;
+		this.dept = dept;
+		this.desg = desg;
+	}
+	public Employee() {
+		
+	}
+
+}
