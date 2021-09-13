104d02d06ecc47e0c1c0451fa9c43a10b3591bf2
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2021-09-13 12:12:46 +0530

    Add files via upload

--- noteseurekaclient/src/main/java/com/example/notes/noteseurekaclient/NoteseurekaclientApplication.java
+++ noteseurekaclient/src/main/java/com/example/notes/noteseurekaclient/NoteseurekaclientApplication.java
@@ -7,8 +7,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
+
+@SuppressWarnings("deprecation")
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableEurekaClient
 @ComponentScan(basePackages="com.example.notes.noteseurekaclient")
 public class NoteseurekaclientApplication {
