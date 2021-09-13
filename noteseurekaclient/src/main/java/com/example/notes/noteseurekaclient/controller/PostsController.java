fdeb7ad8d551b4d666614f6ca5ae8b660f974d5d
praveeniiht <64822037+praveeniiht@users.noreply.github.com>
2021-09-13 12:13:39 +0530

    Add files via upload

--- noteseurekaclient/src/main/java/com/example/notes/noteseurekaclient/controller/PostsController.java
+++ noteseurekaclient/src/main/java/com/example/notes/noteseurekaclient/controller/PostsController.java
@@ -1,5 +1,6 @@
 package com.example.notes.noteseurekaclient.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.notes.noteseurekaclient.model.CommentsDto;
 import com.example.notes.noteseurekaclient.model.Posts;
 import com.example.notes.noteseurekaclient.repo.service.PostsService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -31,8 +33,10 @@ public class PostsController {
 
 	
 	@GetMapping("/test")
+	
 	public List<ServiceInstance> hello() {
-		List<ServiceInstance> insts = client.getInstances("COMMENTSERVICE");
+		List<ServiceInstance> insts = client.getInstances("POST-SERVICE");
+	//	System.out.println(insts.get(1).getHost());)
 		insts.forEach(System.out::println);
 		
 		return insts;
@@ -74,8 +78,18 @@ public class PostsController {
 	}
 	
 	@GetMapping("/comments/{postId}")
+	@HystrixCommand(fallbackMethod="getCommentService")
 	public List<CommentsDto> searchCommentsByPostId(@PathVariable int postId){
 		return (List<CommentsDto>) service.searchCommentsByPostId(postId);
+	}
+	
+	public List<CommentsDto> getCommentService(@PathVariable int postId){
+		CommentsDto comment1 = new CommentsDto(7,5,"kishore kumar","need more on java");
+		CommentsDto comment2 = new CommentsDto(9,5,"sunil krishna","very basic info");
+		List<CommentsDto> data = new ArrayList();
+		data.add(comment1);
+		data.add(comment2);
+		return data;
 	}
 
 }
